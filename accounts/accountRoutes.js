62a73b4c95d4615e8d71e671a891059f9ae78fe6
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-10 01:40:27 -0400

    Created validateAccount middleware. Created /api/accounts/:id DELETE endpoint

--- accounts/accountRoutes.js
+++ accounts/accountRoutes.js
@@ -12,22 +12,35 @@ router.get("/", (req, res, next)=>{
 
 router.post("/", (req, res, next)=>{
     const {name, budget} = req.body;
-    if(!name || !budget) return res.status(400).json({message: "name and budget fields are required"});
+    if(!name || !budget) return res.status(400).json({message: "Name and budget fields are required."});
     db("accounts").insert({name: name, budget: budget}).then(accountId=>{
         res.status(201).json({id: accountId, ...req.body});
     }).catch(err=>{
         next(err);
-    })
-})
+    });
+});
 
-router.get("/:id", (req, res, next)=>{
-    db("accounts").where({id: req.params.id}).then(user=>{
-        user = user[0];
-        if(!user) res.status(404).json({message: "User not found."});
-        res.status(200).json(user);
+router.get("/:id", validateAccount, (req, res, next)=>{
+    res.status(200).json(req.account);
+});
+
+router.delete("/:id", validateAccount, (req, res, next)=>{
+    db("accounts").where({id: req.params.id}).del().then(numOfAccountsDeleted=>{
+        res.status(200).json(req.account);
+    }).catch(err=>{
+        next(err);
+    })
+});
+
+function validateAccount (req, res, next){
+    db("accounts").where({id: req.params.id}).then(account=>{
+        account = account[0];
+        if(!account) return res.status(404).json({message: "Account not found."});
+        req.account = account;
+        next();
     }).catch(err=>{
         next(err);
     });
-});
+}
 
 module.exports = router;
