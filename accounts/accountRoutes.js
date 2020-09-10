8cf73140a1ed555be475fe428ee20fe9920756a9
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-09 23:45:37 -0400

    Created /api/accounts/:id get endpoint

--- accounts/accountRoutes.js
+++ accounts/accountRoutes.js
@@ -7,7 +7,17 @@ router.get("/", (req, res, next)=>{
       res.status(200).json(accounts);
     }).catch(err=>{
       next(err);
-    })
+    });
+});
+
+router.get("/:id", (req, res, next)=>{
+    db("accounts").where({id: req.params.id}).then(user=>{
+        user = user[0];
+        if(!user) res.status(404).json({message: "User not found."});
+        res.status(200).json(user);
+    }).catch(err=>{
+        next(err);
+    });
 });
 
 module.exports = router;
